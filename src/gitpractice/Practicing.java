21ca1edd426e850f39b9e96b7d5e4d9353c5e0f8
jawedsadat <jawedsadat@gmail.com>
2022-11-16 16:58:05 -0800

    This is from class

--- src/gitpractice/Practicing.java
+++ src/gitpractice/Practicing.java
@@ -4,7 +4,11 @@ public class Practicing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	}
+	public void myProject() {
+		System.out.println("This is for branching");
 	}
 
 }
